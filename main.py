bd7286630449942fa0e21bdaeb59c3ca03eeb418
sejoon00 <parksejoon313@gmail.com>
2023-11-29 22:36:08 +0900

    [Fix] 디렉토리 현재 위치로 설정 코드 추가

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os
 import sys
-sys.path.append('..')
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+print(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 import uvicorn
